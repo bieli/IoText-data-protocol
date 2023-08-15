87bfa9710c7845d6d1b2d298bf026d8e0835f64c
Marcin Bielak <marcin.bieli@gmail.com>
2023-08-15 11:24:39 +0200

    fix CI for black code convention

--- benchmarks/IoText_codecs_benchmark_test.py
+++ benchmarks/IoText_codecs_benchmark_test.py
@@ -4,9 +4,9 @@ import json
 
 from src.codecs.iot_ext_codec import IoTextCodec
 
-MSG_1_EXAMPLE = '''t|3900237526042,d|device_name_001,m|val_water_001=i:1234,m|val_water_002=i:15,m|bulb_state=b:1,m|connector_state=b:0,m|temp_01=d:34.4,m|temp_02=d:36.4,m|temp_03=d:10.4,m|pwr=d:12.231,m|current=d:1.429,m|current_battery=d:1.548'''
+MSG_1_EXAMPLE = """t|3900237526042,d|device_name_001,m|val_water_001=i:1234,m|val_water_002=i:15,m|bulb_state=b:1,m|connector_state=b:0,m|temp_01=d:34.4,m|temp_02=d:36.4,m|temp_03=d:10.4,m|pwr=d:12.231,m|current=d:1.429,m|current_battery=d:1.548"""
 MSG_1_EXAMPLE_DECODED = IoTextCodec.decode(MSG_1_EXAMPLE)
-MSG_1_EXAMPLE_JSON = '''[{"t":3900237526042},{"d":"device_name_001"},{"m":[{"val_water_001":{"i":1234}},{"val_water_002":{"i":15}},{"bulb_state":{"b":1}},{"connector_state":{"b":0}},{"temp_01":{"d":"34.4"}},{"temp_02":{"d":"36.4"}},{"temp_03":{"d":"10.4"}},{"pwr":{"d":"12.231"}},{"current":{"d":"1.429"}},{"current_battery": {"d":"1.548"}}]}]'''
+MSG_1_EXAMPLE_JSON = """[{"t":3900237526042},{"d":"device_name_001"},{"m":[{"val_water_001":{"i":1234}},{"val_water_002":{"i":15}},{"bulb_state":{"b":1}},{"connector_state":{"b":0}},{"temp_01":{"d":"34.4"}},{"temp_02":{"d":"36.4"}},{"temp_03":{"d":"10.4"}},{"pwr":{"d":"12.231"}},{"current":{"d":"1.429"}},{"current_battery": {"d":"1.548"}}]}]"""
 
 MIN_TESTS_ROUNDS = 100
 
@@ -18,17 +18,18 @@ MIN_TESTS_ROUNDS = 100
     min_rounds=MIN_TESTS_ROUNDS,
     timer=time.time,
     disable_gc=True,
-    warmup=False
+    warmup=False,
 )
 def test_iotext_data_protocol_decode_python(benchmark):
     @benchmark
-    def result():        
+    def result():
         IoTextCodec.decode(MSG_1_EXAMPLE)
 
     # Extra code, to verify that the run
     # completed correctly.
     # Note: this code is not measured.
     assert result is None
+
 
 @pytest.mark.benchmark(
     group="bench",
@@ -37,11 +38,11 @@ def test_iotext_data_protocol_decode_python(benchmark):
     min_rounds=MIN_TESTS_ROUNDS,
     timer=time.time,
     disable_gc=True,
-    warmup=False
+    warmup=False,
 )
 def test_iotext_data_protocol_encode_python(benchmark):
     @benchmark
-    def result():        
+    def result():
         IoTextCodec.encode(MSG_1_EXAMPLE_DECODED)
 
     # Extra code, to verify that the run
@@ -57,11 +58,11 @@ def test_iotext_data_protocol_encode_python(benchmark):
     min_rounds=MIN_TESTS_ROUNDS,
     timer=time.time,
     disable_gc=True,
-    warmup=False
+    warmup=False,
 )
 def test_json_decode_python(benchmark):
     @benchmark
-    def result():        
+    def result():
         json.loads(MSG_1_EXAMPLE_JSON)
 
     # Extra code, to verify that the run
