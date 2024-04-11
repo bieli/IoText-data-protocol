a615a86d39035e8f183167a276367fdb6b4f1600
Marcin Bielak <marcin.bieli@gmail.com>
2024-04-11 23:01:23 +0200

    add setup.py for install library with pip

--- __init__.py
+++ __init__.py
@@ -1,2 +1,2 @@
 VERSION = (1, 0, 1)
-__version__ = '.'.join(str(x) for x in VERSION)
+__version__ = ".".join(str(x) for x in VERSION)
--- setup.py
+++ setup.py
@@ -7,80 +7,80 @@ from setuptools import setup, find_packages
 here = os.path.abspath(os.path.dirname(__file__))
 
 
-with open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(os.path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 
 def read_version():
-    regexp = re.compile(r'^VERSION\W*=\W*\(([^\(\)]*)\)')
-    init_py = os.path.join(here, '__init__.py')
-    with open(init_py, encoding='utf-8') as f:
+    regexp = re.compile(r"^VERSION\W*=\W*\(([^\(\)]*)\)")
+    init_py = os.path.join(here, "__init__.py")
+    with open(init_py, encoding="utf-8") as f:
         for line in f:
             match = regexp.match(line)
             if match is not None:
-                return match.group(1).replace(', ', '.')
+                return match.group(1).replace(", ", ".")
         else:
             raise RuntimeError(
-                'Cannot find version in __init__.py'
+                "Cannot find version in __init__.py"
             )
 
 
-github_url = 'https://github.com/bieli/IoText-data-protocol'
+github_url = "https://github.com/bieli/IoText-data-protocol"
 
 setup(
-    name='IoText-data-protocol',
+    name="IoText-data-protocol",
     version=read_version(),
 
-    description='IoText data protocol Python library (spec. + serDe + builder)',
+    description="IoText data protocol Python library (spec. + serDe + builder)",
     long_description=long_description,
 
     url=github_url,
 
-    author='Marcin Bielak',
-    author_email='marcin.bieli+IoText+data+protocol@gmail.com',
+    author="Marcin Bielak",
+    author_email="marcin.bieli+IoText+data+protocol@gmail.com",
 
-    license='Apache',
+    license="Apache",
 
     classifiers=[
-        'Development Status :: 4 - Beta',
+        "Development Status :: 4 - Beta",
 
 
-        'Environment :: Console',
+        "Environment :: Console",
 
 
-        'Intended Audience :: Developers',
-        'Intended Audience :: Information Technology',
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
 
 
-        'License :: OSI Approved :: Apache License',
+        "License :: OSI Approved :: Apache License",
 
 
-        'Operating System :: OS Independent',
+        "Operating System :: OS Independent",
 
 
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
 
-        'Topic :: Database',
-        'Topic :: Software Development',
-        'Topic :: Software Development :: Libraries',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Topic :: Scientific/Engineering :: Information Analysis'
+        "Topic :: Database",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Scientific/Engineering :: Information Analysis"
     ],
 
-    keywords='Internet Of Things IoT IIoT data protocol',
+    keywords="Internet Of Things IoT IIoT data protocol",
 
     project_urls={
-        'Documentation': 'https://github.com/bieli/IoText-data-protocol',
-        'Changes': github_url + '/releases'
+        "Documentation": "https://github.com/bieli/IoText-data-protocol",
+        "Changes": github_url + "/releases"
     },
-    packages=find_packages('.', exclude=["tests*"]),
-    python_requires='>=3.7, <4',
+    packages=find_packages(".", exclude=["tests*"]),
+    python_requires=">=3.7, <4",
     install_requires=[],
-    test_suite='pytest'
+    test_suite="pytest"
 )
